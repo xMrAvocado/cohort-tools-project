dc153df507248106f2763a3339ed6127af392ca5
Juan Marin <jmo129@gmail.com>
2025-03-18 18:22:49 +0100

    Mongoose funcionando

--- server/app.js
+++ server/app.js
@@ -7,11 +7,27 @@ const cors = require("cors");
 // STATIC DATA
 // Devs Team - Import the provided files with JSON data of students and cohorts here:
 // ...
-const students = require(`./students.json`);
-const cohorts = require(`./cohorts.json`);
+//const students = require(`./students.json`);
+//const cohorts = require(`./cohorts.json`);
+// Mongoose aquí
+const mongoose = require(`mongoose`);
+const Cohort = require(`./models/Cohort.model`)
+const Student = require(`./models/Student.model`)
+
+// CONECTAMOS MONGOOSE
+mongoose.connect("mongodb://127.0.0.1:27017/cohort-tools-api")
+.then(x => console.log(`Connected to Database`))
+.catch(err => console.error("Error connecting to MongoDB cohorts", err))
+
+/*
+mongoose.connect("mongodb://127.0.0.1:27017/students")
+.then(x => console.log(`Connected to Database: "${x.connections[0].cohort-tools-api}"`))
+.catch(err => console.error("Error connecting to MongoDB students", err))
+*/
 
 // INITIALIZE EXPRESS APP - https://expressjs.com/en/4x/api.html#express
 const app = express();
+
 
 
 // MIDDLEWARE
@@ -35,11 +51,23 @@ app.get("/docs", (req, res) => {
 });
 
 app.get(`/api/cohorts`,(req,res)=>{
-res.json(cohorts);
+  Cohort.find({})
+  .then((cohorts) => {
+    console.log("Estamos dentro del Then Cohorts")
+    res.json(cohorts);})
+  .catch((error) => {
+    console.log("Error",error)
+    res.status(500).json({error: "Failed to retrieve Cohorts"})})
 })
 
 app.get(`/api/students`,(req, res)=>{
-  res.json(students);
+  Student.find({})
+  .then((students) => {
+    console.log("Estamos dentro del Then Students")
+    res.json(students);})
+  .catch((error) => {
+    console.log("Error",error)
+    res.status(500).json({error: "Failed to retrieve Students"})})
 })
 
 // START SERVER
---
+++ server/models/Student.model.js
@@ -0,0 +1,22 @@
+const mongoose = require(`mongoose`);
+
+const Schema = mongoose.Schema;
+
+const studentSchema = new Schema({
+
+    firstName: String,
+    lastName: String,
+    email: String,
+    phone: String,
+    linkedinUrl: String, 
+    languages: [String],
+    program: String,
+    background: String, 
+    image: String, 
+    projects: [String],
+    cohort: mongoose.Schema.Types.ObjectId
+})
+
+const Student = mongoose.model("Student", studentSchema);
+
+module.exports = Student;
